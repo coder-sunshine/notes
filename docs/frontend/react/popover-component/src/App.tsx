0168569394ef4500a7593246ec65434269db89ba
wff <283953455@qq.com>
2024-12-24 17:18:33 +0800

    feat: 🚀 popver气泡卡片组件

---
+++ docs/frontend/react/popover-component/src/App.tsx
@@ -0,0 +1,22 @@
+import Popover from './Popover'
+
+export default function App() {
+  const popoverContent = (
+    <div>
+      testetstas
+      <button
+        onClick={() => {
+          alert(1)
+        }}
+      >
+        111
+      </button>
+    </div>
+  )
+
+  return (
+    <Popover content={popoverContent} placement='bottom' trigger='click' style={{ margin: '200px' }}>
+      <button>点我点我</button>
+    </Popover>
+  )
+}
